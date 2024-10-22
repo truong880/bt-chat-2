5688472e5d315f12cbed5cb688a891a117dae1e2
unknown <nguyendinhtruong15012006@gmail.com>
2024-10-22 08:28:00 +0700

    battap

---
+++ Book2.3.java
@@ -0,0 +1,69 @@
+public class Book
+{
+    private String isbn, name;
+    private Author author;
+    private double price;
+    private int qty = 0;
+
+    public Book(String isbn, String name, Author author, double price)
+    {
+        this.isbn = isbn;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String isbn, String name, Author author, double price, int qty)
+    {
+        this.isbn = isbn;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getIsbn()
+    {
+        return isbn;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Author getAuthor()
+    {
+        return author;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public int getQty()
+    {
+        return qty;
+    }
+
+    public void setQty(int qty)
+    {
+        this.qty = qty; 
+    }
+
+    public String getAuthorName()
+    {
+        return author.toString();
+    }
+
+    public String toString()
+    {
+        return "Book[name = " + name + ", " + author + ", price = " + price + ", qty = " + qty + "]";
+    }
+}
